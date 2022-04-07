c9df3201deb0e3189f12bc9c10ace9a1b15fc6bd
narendrapatidar <nk580585@gmail.com>
2022-04-07 21:27:13 +0530

    this is a another branch

--- src/routes/route.js
+++ src/routes/route.js
@@ -6,27 +6,30 @@ const util3 = require('../util/helper')
 const validator1 = require('../validator/formatter')
 const validator2 = require('../validator/formatter')
 const validator3 = require('../validator/formatter')
-const lodash = require ("lodash")
+const lodash = require("lodash")
 const router = express.Router();
 // const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 // console.log( _.chunk(months,4))
 // const arr1 =[1,3,5,7,9,11,13,15,17,19]
 router.get('/test-me', function (req, res) {
- 
+    let getbatch=req.params.batch
+     console.log(getbatch)
     res.send('My first ever bhalot!')
     // const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
     //    console.log( _.chunk(months,4))
-        // console.log( _.tail(arr1))
-     console.log("calling the logger")
+    // console.log( _.tail(arr1))
+    console.log("calling the logger")
     logger.logging()
-    console.log(" calling the util",util.util1)
-    console.log(" calling the util",util2.util2)
-    console.log(" calling the util",util3.util3)
-    console.log(" calling the validator",validator1.validator1)
-    console.log(" calling the validator",validator2.validator2)
-    console.log(" calling the validator",validator3.validator3)
+    console.log(" calling the util", util.util1)
+    console.log(" calling the util", util2.util2)
+    console.log(" calling the util", util3.util3)
+    console.log(" calling the validator", validator1.validator1)
+    console.log(" calling the validator", validator2.validator2)
+    console.log(" calling the validator", validator3.validator3)
 
 });
+
+
 
 module.exports = router;
 // adding this comment for no   
