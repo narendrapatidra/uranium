b54dc09fb119d5b772cc684f70327f1ad723c78c
Karan039 <krndhamija9@gmail.com>
2022-05-04 14:27:28 +0530

    internModel

--- src/models/internModel.js
+++ src/models/internModel.js
@@ -1,13 +1,52 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose")
+const emailValidator = require("email-validator")
+const ObjectId = mongoose.Schema.Types.ObjectId
+const objectId = mongoose.Types.ObjectId
 
 const internSchema = new mongoose.Schema({
-     name: {
-         mandatory}, 
-    email: {mandatory, valid email, unique}, 
-    mobile: {mandatory, valid mobile number, unique}, 
-    collegeId: {ObjectId, ref to college model}, 
+    name: {
+        type: String,
+        required: "Name is required",
+        trim: true
+    },
+    email: {
+        type: string,
+        required: true,
+        unique: true,
+        trim: true,
+        validate: {
+            validator: function (email) {
+                return emailValidator.validate(email)
+            }, message: "Enter valid email id", isAsync: false
+        }
+    },
+    mobile: {
+        type: number,
+        required: "Mobile number is required",
+        unique: true,
+        trim: true,
+        validate: {
+            validator: function (number) {
+                let x = number.toString()
+                return ((x[0] == 9 || x[0] == 8 || x[0] == 7 || x[0] == 6) && x.length == 10 && x !== "")
+            }, message: "Enter valid mobile number", isAsync: false
+        }
+    },
+    collegeId: {
+        type: ObjectId,
+        trim: true,
+        validate: {
+            validator: function (CollegeId) {
+                return objectId.isValid(CollegeId)
+            }, message: "Enter valid collegeId", isAsync: false
+
+        },
+        ref: "College"
+    },
     isDeleted: {
-        type: boolean, 
+        type: boolean,
         default: false
     }
-})
+}, { timestamps: true })
+
+module.exports = mongoose.model("Intern", internSchema)
