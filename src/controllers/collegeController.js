e1305f2cbfd2ff6d6174600d48a7d15b323f7839
Karan039 <krndhamija9@gmail.com>
2022-05-04 16:27:57 +0530

    done

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -1,21 +1,39 @@
-const collegeModel = require('../models/collegeModel')
+const { find } = require('../models/collegeModel');
+const collegeModel = require('../models/collegeModel');
+const internModel = require('../models/internModel');
 
-const createCollege = async function(req,res){
+//1.
+const createCollege = async function (req, res) {
     try {
- let body = req.body;
+        let body = req.body;
 
- if(Object.keys(body)==0){
-     return res.status(400).send({status:false ,msg:"data require in body"})
- }
+        if (Object.keys(body) == 0) {
+            return res.status(400).send({ status: false, msg: "data require in body" })
+        }
 
- const college = await collegeModel.create(body);
-  res.status(201).send({ status:true , msg : " college creat successfilly" ,data : college})
+        const college = await collegeModel.create(body);
+        res.status(201).send({ status: true, msg: " college creat successfilly", data: college })
+    }
+    catch (err) {
+        res.status(500).send({ status: false, error: err.message })
+    }
+
 }
-catch(err)
-{
-    res.status(500).send({status:false,error:err.message})
+
+
+//3.
+const getColleges = async function (req, res) {
+    try {
+        let data = req.query
+        let collegeDetail = await collegeModel.findOne({ name: data.collegeName })
+        let internDetail = await internModel.find({ collegeId: collegeDetail._id }).select({name:1,email:1,mobile:1})
+        collegeDetail = await collegeModel.findOne({ name: data.collegeName }).select({name:1,fullName:1,logoLink:1,_id:0})
+        collegeDetail._doc["interests"]=internDetail
+        res.status(200).send({ status: true, data: collegeDetail})
+    } 
+    catch (err) {
+        res.status(500).send({ status: false, error: err.message })
+    } 
 }
 
-}
-
-module.exports  = { createCollege }
+module.exports = { createCollege, getColleges }
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { createCollege } = require('../controllers/collegeController');
+const { createCollege, getColleges } = require('../controllers/collegeController');
 const router = express.Router();
 const { createIntern } = require("../controllers/internController")
 
 router.post("/functionup/interns", createIntern)
 router.post("/functionup/colleges", createCollege)
-
+router.get("/functionup/collegeDetails", getColleges)
 
 module.exports = router;
