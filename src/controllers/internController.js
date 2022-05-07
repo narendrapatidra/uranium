1fa73a732f2ae07e48485dbae23bb84f8b5e9654
Karan039 <krndhamija9@gmail.com>
2022-05-07 19:14:49 +0530

    internship

--- src/controllers/internController.js
+++ src/controllers/internController.js
@@ -1,6 +1,4 @@
 const internModel = require("../models/internModel")
-const mongoose = require("mongoose")
-const objectId = mongoose.Types.ObjectId
 const emailValidator = require("email-validator")
 const collegeModel = require('../models/collegeModel');
 
@@ -8,65 +6,78 @@ const collegeModel = require('../models/collegeModel');
 const createIntern = async function (req, res) {
     try {
         let data = req.body;
-        let keys = Object.keys(data)
+        let getEmail = await internModel.findOne({ email: data.email })
+        let getMobile = await internModel.findOne({ mobile: data.mobile })
 
         function isPresent(value) {
             if (!value || value.trim().length == 0)
                 return true
         }
 
-        //when body is empty
-        if (keys.length == 0)
-            return res.status(400).send({ status: false, msg: "data is required in body" })
+        function badRequest() {
+            let error = [];
 
-        //checks if name is not given in body
-        if (isPresent(data.name))
-            return res.status(400).send({ status: false, msg: "name is required in body" })
+            //when body is empty
+            if (Object.keys(data).length == 0)
+                return "data is required in body"
 
-        //checks if email is not given in bodyeturn res.st
-        if (isPresent(data.email))
-            return res.status(400).send({ status: false, msg: "email is required in body" })
+            //checks if name is not given in body
+            if (isPresent(data.name))
+                error.push("name is required in body")
 
-        //checks if email id is already present in collection
-        if (await internModel.findOne({ email: data.email }))
-            res.status(404).send({ status: false, msg: "email Id is already in use" })
+            //checks if email is not given in bodyeturn res.st
+            if (isPresent(data.email))
+                error.push("email is required in body")
 
-        //checks email validity
-        if (!emailValidator.validate(data.email))
-            return res.status(400).send({ status: false, msg: "enter valid email id" })
+            //checks if email id is already present in collection
+            if (getEmail)
+                error.push("email Id is already in use")
 
-        //checks if mobile is not given in body
-        if (isPresent(data.mobile))
-            return res.status(400).send({ status: false, msg: "mobile number is required in body" })
+            //checks email validity 
+            if (data.email && !emailValidator.validate(data.email))
+                error.push("enter valid email id")
 
-        //checks for valid mobile number
-        function isValid(x, y) {
-            if (isNaN(y))
-                return true;
-            else if ((x[0] == 9 || x[0] == 8 || x[0] == 7 || x[0] == 6) && x.length == 10)
-                return false;
-            else return true;
+            //checks if mobile is not given in body
+            if (isPresent(data.mobile))
+                error.push("mobile number is required in body")
+
+            //checks for valid mobile number
+            function isValid(x, y) {
+                if (isNaN(y))
+                    return true;
+                else if ((x[0] == 9 || x[0] == 8 || x[0] == 7 || x[0] == 6) && x.length == 10)
+                    return false;
+                else return true;
+            }
+            let y = parseInt(data.mobile)
+            let x = y.toString()
+            if (data.mobile && isValid(x, y))
+                error.push("Enter valid mobile number")
+
+            //check unique mobile
+            if (getMobile)
+                error.push("mobile number is already in use")
+
+            //checks if collegeName is given in body
+            if (isPresent(data.collegeName))
+                error.push("collegeName is required")
+
+            if (error.length > 0)
+                return error;
         }
-        let y = parseInt(data.mobile)
-        let x = y.toString()
-        if (isValid(x, y))
-            return res.status(400).send({ status: false, msg: "Enter valid mobile number" })
 
-        //check unique mobile
-        if (await internModel.findOne({ mobile: data.mobile }))
-            return res.status(400).send({ status: false, msg: "mobile number is already in use" })
+        if (badRequest()) {
+            let err = badRequest();
+            return res.status(400).send({ status: false, msg: err })
+        }
 
-        //checks if CollegeId is given in body
-        if (isPresent(data.collegeName))
-            return res.status(400).send({ status: false, msg: "collegeName is required" })
-
-        let  Name = await collegeModel.findOne({name : data.collegeName, isDeleted : false})
-        if (!Name) return res.status(400).send({status : false, msg: "college not found"})
-        data.collegeId = Name._id 
+        let Name = await collegeModel.findOne({ name: data.collegeName, isDeleted: false })
+        if (!Name)
+            return res.status(404).send({ status: false, msg: "college not found" })
+        data.collegeId = Name._id
 
         let internData = await internModel.create(data)
         res.status(201).send({ status: true, data: internData })
-
     }
     catch (error) {
         res.status(500).send({ status: false, message: error.message })
